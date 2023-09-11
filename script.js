cdeb919e153962156f835e96ffad1feb07ee341b
Jason <jason.miguel022@gmail.com>
2023-09-11 19:47:20 -0300

    Entrega Final

---
+++ script.js
@@ -0,0 +1,47 @@
+// Não altere o nome das variavéis ou o texto do prompt! 
+// Modifique apenas o tipo da variavel!            
+var gender = prompt('Genero');
+var height = prompt('Altura');
+var barReps = prompt('Repetições com barra');
+var barSeconds = prompt('Tempo das repetições com barra');
+var abs = prompt('Abdominais');
+var runDistance = prompt('Distância da corrida');
+var runTime = prompt('Tempo da corrida');
+var swimDistance = prompt('Distância da natação');
+var swimTime = prompt('Tempo da natação');
+var diveTime = prompt(' Tempo de mergulho');
+
+var result = false;
+
+
+if(gender == "male"){
+    let gender = prompt('male');
+    let height = prompt('1.70 metros');
+    let barReps = prompt('6 repetiçõess');
+    let barSeconds = prompt('15 segundos');
+    let abs = prompt('41 abdominais');
+    let runDistance = prompt('3km 5km');
+    let runTime = prompt('12 minutos 20 minutos');
+    let swimDistance = prompt('100 metros');
+    let swimTime = prompt('1 minuto');
+    let diveTime = prompt('30 segundos');
+
+var result = false;
+    
+}
+
+else if(gender == "female"){
+    let gender = prompt('female');
+    let height = prompt('1.60 metros');
+    let barReps = prompt('5 repetiçõess');
+    let barSeconds = prompt('12 segundos');
+    let abs = prompt('41 abdominais');
+    let runDistance = prompt('4km 6km');
+    let runTime = prompt('15 minutos 22 minutos');
+    let swimDistance = prompt('100 metros');
+    let swimTime = prompt('1 minuto');
+    let diveTime = prompt('30 segundos');
+
+var result = true;
+    
+}
